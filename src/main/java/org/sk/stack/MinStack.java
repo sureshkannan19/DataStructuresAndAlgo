21a0526c18634911bf4fb9f93268ebbbe56cbf60
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2023-05-02 22:10:09 +0530

    MinStack and MinStackViaNode - solved

---
+++ src/main/java/org/sk/stack/MinStack.java
@@ -0,0 +1,76 @@
+package org.sk.stack;
+
+import java.util.LinkedList;
+
+public class MinStack {
+
+	LinkedList<Integer> stack;
+	LinkedList<Integer> minStack;
+	int min = 0;
+
+	public MinStack() {
+		stack = new LinkedList<>();
+		minStack = new LinkedList<>();
+	}
+
+	public void push(int val) {
+		stack.push(val);
+		if (!minStack.isEmpty()) {
+			min = Math.min(val, min);
+		} else {
+			min = val;
+		}
+		minStack.push(min);
+	}
+
+	public void pop() {
+		stack.poll();
+		Integer value = minStack.poll();
+		if (value != null && value.equals(min) && !minStack.isEmpty()) {
+			min = minStack.peek();
+		}
+	}
+
+	public int top() {
+		return stack.peek();
+	}
+
+	public int getMin() {
+		return minStack.peek();
+	}
+
+	public static void main(String[] args) {
+		MinStack ms = new MinStack();
+		ms.push(2);
+		ms.push(0);
+		ms.push(3);
+		ms.push(0);
+		System.out.println(ms.getMin());
+		ms.pop();
+		System.out.println(ms.getMin());
+		ms.pop();
+		System.out.println(ms.getMin());
+		ms.pop();
+		System.out.println(ms.getMin());
+
+		MinStack ms1 = new MinStack();
+		ms1.push(-10);
+		ms1.push(14);
+		System.out.println(ms1.getMin());
+		System.out.println(ms1.getMin());
+		ms1.push(-20);
+		System.out.println(ms1.getMin());
+		System.out.println(ms1.getMin());
+		System.out.println(ms1.top());
+		System.out.println(ms1.getMin());
+		ms1.pop();
+		ms1.push(10);
+		ms1.push(-7);
+		System.out.println(ms1.getMin());
+		ms1.push(-7);
+		ms1.pop();
+		System.out.println(ms1.top());
+		System.out.println(ms1.getMin());
+		ms1.pop();
+	}
+}
---
+++ src/main/java/org/sk/stack/MinStackViaNode.java
@@ -0,0 +1,90 @@
+package org.sk.stack;
+
+class Node {
+
+	int value;
+	Node next;
+	int min;
+
+	Node() {
+	}
+
+	Node(int value) {
+		this.value = value;
+	}
+}
+
+public class MinStackViaNode {
+
+	Node stack;
+	int size;
+
+	public MinStackViaNode() {
+		stack = new Node();
+		size = 0;
+	}
+
+	public void push(int val) {
+		Node tmp = new Node(val);
+		if (size == 0) {
+			stack = tmp;
+			stack.value = val;
+			stack.next = null;
+			stack.min = val;
+			size++;
+		} else {
+			tmp.min = Math.min(stack.min, val);
+			tmp.next = stack;
+			stack = tmp;
+			size++;
+		}
+	}
+
+	public void pop() {
+		stack = stack.next;
+		size--;
+	}
+
+	public int top() {
+		return stack.value;
+	}
+
+	public int getMin() {
+		return stack.min;
+	}
+
+	public static void main(String[] args) {
+		MinStackViaNode ms = new MinStackViaNode();
+		ms.push(2);
+		ms.push(0);
+		ms.push(3);
+		ms.push(0);
+		System.out.println(ms.getMin());
+		ms.pop();
+		System.out.println(ms.getMin());
+		ms.pop();
+		System.out.println(ms.getMin());
+		ms.pop();
+		System.out.println(ms.getMin());
+
+		MinStackViaNode ms1 = new MinStackViaNode();
+		ms1.push(-10);
+		ms1.push(14);
+		System.out.println(ms1.getMin());
+		System.out.println(ms1.getMin());
+		ms1.push(-20);
+		System.out.println(ms1.getMin());
+		System.out.println(ms1.getMin());
+		System.out.println(ms1.top());
+		System.out.println(ms1.getMin());
+		ms1.pop();
+		ms1.push(10);
+		ms1.push(-7);
+		System.out.println(ms1.getMin());
+		ms1.push(-7);
+		ms1.pop();
+		System.out.println(ms1.top());
+		System.out.println(ms1.getMin());
+		ms1.pop();
+	}
+}
