edfa6dda2e6bf0eded0cf22116a3a2cd7c016406
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2023-05-03 10:04:31 +0530

    refractored

--- src/main/java/org/sk/stack/EvaluateReversePolishNotation.java
+++ src/main/java/org/sk/stack/EvaluateReversePolishNotation.java
@@ -35,6 +35,7 @@ public class EvaluateReversePolishNotation {
 	public static void main(String[] args) {
 		EvaluateReversePolishNotation erpn = new EvaluateReversePolishNotation();
 		System.out.println(erpn.evalRPN(new String[] { "2", "1", "+", "3", "*" }));
+		System.out.println(erpn.evalRPN(new String[] { "10","6","9","3","+","-11","*","/","*","17","+","5","+"}));
 		System.out.println(erpn.evalRPN(new String[] { "4", "13", "5", "/", "+" }));
 		System.out.println(erpn.evalRPN(new String[] { "4", "3", "-" }));
 	}
