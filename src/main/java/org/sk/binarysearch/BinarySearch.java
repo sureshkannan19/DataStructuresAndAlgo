9400b4854e5ea40b1fbaf61c52acda22935e3ec4
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2023-11-10 12:02:25 +0530

    BinarySearch.java refactored

--- src/main/java/org/sk/binarysearch/BinarySearch.java
+++ src/main/java/org/sk/binarysearch/BinarySearch.java
@@ -3,36 +3,19 @@ package org.sk.binarysearch;
 public class BinarySearch {
 
 	public int search(int[] nums, int target) {
-
 		int left = 0;
 		int right = nums.length - 1;
 		int mid = right / 2;
 
-		if (target < nums[left] || target > nums[right])
-			return -1;
-
-		if (nums[left] == target)
-			return left;
-		if (nums[right] == target)
-			return right;
-		if (nums[mid] == target)
-			return mid;
-
 		while (left <= right) {
-			mid = left + (right - left) / 2;
 			if (nums[mid] == target) {
 				return mid;
-			}
-			if (target < nums[mid]) {
-				right = mid - 1;
-			} else {
+			} else if (nums[mid] < target) {
 				left = mid + 1;
+			} else if (nums[mid] > target) {
+				right = mid - 1;
 			}
-			if (nums[left] == target) {
-				return left;
-			} else if (nums[right] == target) {
-				return right;
-			}
+			mid = ((right - left) / 2) + left;
 		}
 		return -1;
 	}
