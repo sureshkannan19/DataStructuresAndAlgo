79c200e07160cb8063e02f7fd6e1e0bff147514c
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2023-11-10 12:21:12 +0530

    TwoDBinarySearch.java refactored

--- src/main/java/org/sk/binarysearch/TwoDBinarySearch.java
+++ src/main/java/org/sk/binarysearch/TwoDBinarySearch.java
@@ -2,45 +2,32 @@ package org.sk.binarysearch;
 
 public class TwoDBinarySearch {
 
-	public boolean searchMatrix(int[][] matrix, int target) {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int colLeft, colRight, mid = 0;
 
-		if (target < matrix[0][0] || target > matrix[matrix.length - 1][matrix[0].length - 1]) {
-			return false;
-		}
+        for (int[] ints : matrix) {
+            if (ints[ints.length - 1] < target) {
+                continue;
+            }
+            colLeft = 0;
+            colRight = ints.length - 1;
+            mid = (colRight - colLeft) / 2;
+            while (colLeft <= colRight) {
+                if (ints[mid] == target) {
+                    return true;
+                } else if (ints[mid] < target) {
+                    colLeft = mid + 1;
+                } else {
+                    colRight = mid - 1;
+                }
+                mid = colLeft + (colRight - colLeft) / 2;
+            }
+        }
+        return false;
+    }
 
-		int left = 0;
-		int right = matrix[0].length - 1;
-
-		int row = findRow(matrix, 0, target);
-
-		while (left <= right) {
-			int mid = left + (right - left) / 2;
-			if (matrix[row][mid] == target) {
-				return true;
-			}
-			if (target > matrix[row][mid]) {
-				left = mid + 1;
-			}
-			if (target < matrix[row][mid]) {
-				right = mid - 1;
-			}
-		}
-		return false;
-	}
-
-	private int findRow(int[][] matrix, int row, int target) {
-		if (row <= matrix.length - 1) {
-			if (target <= matrix[row][matrix[0].length - 1]) {
-				return row;
-			}
-			return findRow(matrix, row + 1, target);
-		}
-		// will not happen
-		return -1;
-	}
-
-	public static void main(String[] args) {
-		TwoDBinarySearch tbs = new TwoDBinarySearch();
-		System.out.println(tbs.searchMatrix(new int[][] { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 60 } }, 5));
-	}
+    public static void main(String[] args) {
+        TwoDBinarySearch tbs = new TwoDBinarySearch();
+        System.out.println(tbs.searchMatrix(new int[][]{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}}, 5));
+    }
 }
