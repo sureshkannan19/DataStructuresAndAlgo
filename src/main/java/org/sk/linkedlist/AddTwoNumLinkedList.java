83bc995c34fccec5b01d5d1744b6d68f876c3ebc
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2023-11-19 11:29:06 +0530

    AddTwoNumLinkedList refactored

--- src/main/java/org/sk/linkedlist/AddTwoNumLinkedList.java
+++ src/main/java/org/sk/linkedlist/AddTwoNumLinkedList.java
@@ -2,46 +2,43 @@ package org.sk.linkedlist;
 
 public class AddTwoNumLinkedList {
 
-	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int sum = 0;
+        ListNode result = new ListNode();
+        ListNode temp = result;
 
-		int sum = 0;
-		int rem = 0;
-		ListNode dummy = new ListNode();
-		ListNode next = dummy;
+        int bal = 0;
+        while (l1 != null || l2 != null) {
+            temp.next = new ListNode();
+            temp = temp.next;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            sum += bal;
+            if (sum > 9) {
+                temp.val = sum - 10;
+                bal = 1;
+            } else {
+                temp.val = sum;
+                bal = 0;
+            }
+            sum = 0;
+        }
+        if (bal == 1) {
+            temp.next = new ListNode(1);
+        }
+        return result.next;
+    }
 
-		while (l1 != null || l2 != null) {
-			next.next = new ListNode();
-			sum = 0;
-			if (l1 != null) {
-				sum += l1.val;
-				l1 = l1.next;
-			}
-			if (l2 != null) {
-				sum += l2.val;
-				l2 = l2.next;
-			}
-			sum = rem + sum;
-			if (sum > 9) {
-				sum = sum % 10;
-				rem = 1;
-			} else {
-				rem = 0;
-			}
-			next.next.val = sum;
-			next = next.next;
-		}
-
-		if (rem != 0) {
-			next.next = new ListNode(rem);
-		}
-		return dummy.next;
-	}
-
-	public static void main(String[] args) {
-		AddTwoNumLinkedList adnum = new AddTwoNumLinkedList();
-		ListNode.print(adnum.addTwoNumbers(new ListNode(2, new ListNode(4, new ListNode(3))),
-				new ListNode(5, new ListNode(6, new ListNode(4)))));
-
-	}
+    public static void main(String[] args) {
+        AddTwoNumLinkedList adnum = new AddTwoNumLinkedList();
+		ListNode.print(adnum.addTwoNumbers(new ListNode(2).addAll(4, 3), new ListNode(5).addAll(6, 4)));
+		ListNode.print(adnum.addTwoNumbers(new ListNode(9).addAll(9, 9, 9, 9, 9, 9), new ListNode(9).addAll(9, 9, 9)));
+    }
 
 }
