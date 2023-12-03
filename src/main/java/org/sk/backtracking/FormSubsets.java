ba8c22de6568e137fb98f807db06e6eac1377c1b
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2023-12-03 21:20:48 +0530

    FormSubsets.java solved

---
+++ src/main/java/org/sk/backtracking/FormSubsets.java
@@ -0,0 +1,32 @@
+package org.sk.backtracking;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class FormSubsets {
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        formSubset(nums, result, new LinkedList<>(), 0);
+        return result;
+    }
+
+    private void formSubset(int[] nums, List<List<Integer>> result,
+                            LinkedList<Integer> subset, int i) {
+        if (i >= nums.length) {
+            result.add(new ArrayList<>(subset));
+            return;
+        }
+        subset.add(nums[i]);
+        formSubset(nums, result, subset, i + 1);
+
+        subset.pollLast();
+        formSubset(nums, result, subset, i + 1);
+    }
+
+    public static void main(String[] args) {
+        FormSubsets fs = new FormSubsets();
+        System.out.println(fs.subsets(new int[]{1, 2, 3}));
+    }
+}
