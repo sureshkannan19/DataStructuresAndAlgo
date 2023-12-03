e8dca919a2eac12d60292328517a4e67b2339fa0
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2023-12-03 14:00:32 +0530

    TreeNode.java removed unused import

--- src/main/java/org/sk/trees/FindMaxDepthOfBinaryTree.java
+++ src/main/java/org/sk/trees/FindMaxDepthOfBinaryTree.java
@@ -4,48 +4,43 @@ import java.util.LinkedList;
 
 public class FindMaxDepthOfBinaryTree {
 
-	public int maxDepthViaIteration(TreeNode root) {
-		int depth = 0;
-		
-		LinkedList<Pair> tree = new LinkedList<>();
-		tree.push(new Pair(1, root));
+    public int maxDepthViaIteration(TreeNode root) {
+        int depth = 0;
 
-		while (!tree.isEmpty()) {
-			Pair temp = tree.pop();
-			if (temp.root != null) {
-				depth = Math.max(depth , temp.depth);
-				tree.push(new Pair(temp.depth + 1, temp.root.right));
-				tree.push(new Pair(temp.depth + 1, temp.root.left));
-			}
-		}
-		return depth;
-	}
-	
-	private int maxDepthViaRecursive(TreeNode root, int depth) {
-		/**	
-		 * 				 4
-		 * 				 |
-		 * 			   /   \
-		 * 			  2     7
-		 * 		     / \   / \
-		 * 		    1   3 6   9
-		 */
-		if (root != null) {
-			int left = maxDepthViaRecursive(root.left, depth);
-//			System.out.println("Left val : " + left);
-			int right = maxDepthViaRecursive(root.right, depth);
-//			System.out.println("Right val : " + right);
-			depth = 1 + Math.max(left, right);
-		}
-		return depth;
-	}
+        LinkedList<Pair> tree = new LinkedList<>();
+        tree.push(new Pair(1, root));
 
-	public static void main(String[] args) {
-		FindMaxDepthOfBinaryTree ibt = new FindMaxDepthOfBinaryTree();
-		TreeNode root = new TreeNode(4);
-		root.left = new TreeNode(2, new TreeNode(1), new TreeNode(3));
-		root.right = new TreeNode(6, new TreeNode(5), new TreeNode(7));
-		System.out.println(ibt.maxDepthViaIteration(root));
-		System.out.println(ibt.maxDepthViaRecursive(root, 0));
-	}
+        while (!tree.isEmpty()) {
+            Pair temp = tree.pop();
+            if (temp.root != null) {
+                depth = Math.max(depth, temp.depth);
+                tree.push(new Pair(temp.depth + 1, temp.root.right));
+                tree.push(new Pair(temp.depth + 1, temp.root.left));
+            }
+        }
+        return depth;
+    }
+
+     public int maxDepthViaRecursive(TreeNode root) {
+        return maxDepth(root, 1, 0);
+    }
+
+    private int maxDepth(TreeNode root, int curLevel, int maxLevel) {
+        if (root != null) {
+            maxLevel = Math.max(curLevel, maxLevel);
+            maxLevel = maxDepth(root.left, curLevel + 1, maxLevel);
+            maxLevel = maxDepth(root.right, curLevel + 1, maxLevel);
+        }
+        return maxLevel;
+    }
+
+
+    public static void main(String[] args) {
+        FindMaxDepthOfBinaryTree ibt = new FindMaxDepthOfBinaryTree();
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2, new TreeNode(1), new TreeNode(3));
+        root.right = new TreeNode(6, new TreeNode(5), new TreeNode(7));
+        System.out.println(ibt.maxDepthViaIteration(root));
+        System.out.println(ibt.maxDepthViaRecursive(root));
+    }
 }
