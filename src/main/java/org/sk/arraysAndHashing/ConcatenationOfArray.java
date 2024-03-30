3903608520ffa428ea433405675b38dba7d2db4e
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2024-03-30 19:21:10 +0530

    ConcatenationOfArray Solved

---
+++ src/main/java/org/sk/arraysAndHashing/ConcatenationOfArray.java
@@ -0,0 +1,13 @@
+package org.sk.arraysAndHashing;
+
+public class ConcatenationOfArray {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n * 2];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        return ans;
+    }
+}
