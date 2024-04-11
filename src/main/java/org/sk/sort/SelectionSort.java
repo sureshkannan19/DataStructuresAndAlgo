e0b69b657186d5a2a0b43f15052500f8d0a4413a
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2024-04-11 18:40:32 +0530

    SelectionSort.java

---
+++ src/main/java/org/sk/sort/SelectionSort.java
@@ -0,0 +1,30 @@
+package org.sk.sort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    public int[] sort(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int min = nums[i];
+            int swapInd = i;
+            for (int j = i; j < nums.length; j++) {
+                if (nums[j] < min) {
+                    swapInd = j;
+                    min = nums[j];
+                }
+            }
+            nums[swapInd] = nums[i]; // whatever is in current index move to where the min element found and vice versa
+            nums[i++] = min;
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        SelectionSort ss = new SelectionSort();
+        System.out.println(Arrays.toString(ss.sort(new int[]{5, 4, 2, 3, 1})));
+        System.out.println(Arrays.toString(ss.sort(new int[]{50, 58, 1, 27, 123, 905, 12})));
+    }
+
+}
