ddea1142e3358436996e22df6427258cd04d41e4
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2024-04-11 13:12:10 +0530

    NumberOfStudentsUnableToEatLunch.java solved

--- src/main/java/org/sk/linkedlist/NumberOfStudentsUnableToEatLunch.java
+++ src/main/java/org/sk/linkedlist/NumberOfStudentsUnableToEatLunch.java
@@ -1,5 +1,10 @@
 package org.sk.linkedlist;
 
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.Queue;
+import java.util.Stack;
+
 public class NumberOfStudentsUnableToEatLunch {
     public int countStudents(int[] students, int[] sandwiches) {
         // circular - 0
@@ -20,5 +25,35 @@ public class NumberOfStudentsUnableToEatLunch {
             }
         }
         return 0;
+    }
+
+    public int countStudents1(int[] students, int[] sandwiches) {
+        Queue<Integer> stu = new LinkedList<>();
+        Stack<Integer> san = new Stack<>();
+        for (int i = 0; i < students.length; i++) {
+            stu.offer(students[i]);
+            san.push(sandwiches[sandwiches.length - 1 - i]);
+        }
+        int noOfstudentsNotAteSandwich = 0;
+        // exit the loop --> Number of students unable to eat the sandwich in prior iteration and current iteration remains same
+        while (noOfstudentsNotAteSandwich != stu.size()) {
+            noOfstudentsNotAteSandwich = stu.size();
+            for (int i = 0; i < stu.size(); i++) {
+                if (Objects.equals(stu.peek(), san.peek())) {
+                    stu.poll();
+                    san.pop();
+                    i--; // To process all(last entry) students
+                } else {
+                    stu.offer(stu.poll());
+                }
+            }
+        }
+        return stu.size();
+    }
+
+    public static void main(String[] args) {
+        NumberOfStudentsUnableToEatLunch n = new NumberOfStudentsUnableToEatLunch();
+        n.countStudents1(new int[]{1, 1, 0, 0}, new int[]{0, 1, 0, 1});
+        n.countStudents1(new int[]{1, 1, 1, 0, 0, 1}, new int[]{1, 0, 0, 0, 1, 1});
     }
 }
