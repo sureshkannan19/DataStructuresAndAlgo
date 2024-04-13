e1dcc402eee8f46c0ac391b0938acfcb20a18c50
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2024-04-13 13:05:38 +0530

    Updated QuickSort.java

--- src/main/java/org/sk/sort/QuickSort.java
+++ src/main/java/org/sk/sort/QuickSort.java
@@ -19,7 +19,7 @@ public class QuickSort {
     }
 
     private static int partition(int[] nums, int low, int high) {
-        int pivot = nums[low];
+        int pivot = nums[low]; // taking first value as pivot
         int i = low;
         int j = high;
         while (i < j) {
@@ -29,15 +29,21 @@ public class QuickSort {
             while (nums[j] > pivot && j > low) { // breaking point when element present right side is less than pivot
                 j--;
             }
-            if (i < j) { // swap both i and j elements
+            System.out.print("Before sorting: " + Arrays.toString(nums) + " , Ith val: " + i + " , Jth val: " + j);
+            // i < j --> 5 < 4 --> where index 5 is on the right side of pivot and index 4 is the pivot/partition index, threshold reached, hence stop the loop
+            if (i < j) {
+                // swapping element higher than pivot to right and lower than pivot to left of the pivot
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
             }
+            System.out.println(" , After Sorting: " + Arrays.toString(nums));
         }
+        System.out.print("Before sorting: " + Arrays.toString(nums) + " , Ith val: " + i + " , Jth val: " + j);
         nums[low] = nums[j];
         nums[j] = pivot;
-        return j; // Based on partition index where arrays will be divided into sub array
+        System.out.println(" , After Sorting: " + Arrays.toString(nums));
+        return j; // Partition index where values on the left side are smaller and values on the right side are greater, based on this array will be divided into sub array
     }
 
     public static void main(String[] args) {
