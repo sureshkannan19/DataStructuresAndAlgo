9c0f5d5cb861f521596279c10d13583abd54856b
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2024-04-14 11:14:37 +0530

    Updated SortColor.java

--- src/main/java/org/sk/sort/SortColor.java
+++ src/main/java/org/sk/sort/SortColor.java
@@ -19,8 +19,35 @@ public class SortColor {
         System.out.println(Arrays.toString(nums));
     }
 
+    public void sortColorsViaTwoPass(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        int i = 0;
+        while (i <= right) {
+            if (nums[i] == 0) {
+                swap(nums, i, left);
+                left++;
+            } else if (nums[i] == 2) {
+                swap(nums, i, right);
+                right--;
+                i--;
+            }
+            i++;
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[j];
+        nums[j] = nums[i];
+        nums[i] = temp;
+    }
+
     public static void main(String[] args) {
         SortColor s = new SortColor();
+        s.sortColorsViaTwoPass(new int[]{2, 0, 2, 1, 1, 0});
+        s.sortColorsViaTwoPass(new int[]{1, 2, 0});
+
         s.sortColors(new int[]{2, 0, 2, 1, 1, 0});
+        s.sortColors(new int[]{1, 2, 0});
     }
 }
