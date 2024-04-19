0cb3d3301617e2a6af4e6ae25917acf036b8b996
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2024-04-19 17:12:22 +0530

    Updated DeleteNodeBinaryTree.java

---
+++ src/main/java/org/sk/trees/DeleteNodeBinaryTree.java
@@ -0,0 +1,85 @@
+package org.sk.trees;
+
+public class DeleteNodeBinaryTree {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null)
+            return null;
+
+        TreeNode tobeDeleted = root;
+        TreeNode prev = root;
+
+        boolean left = false;
+        boolean right = false;
+        while (tobeDeleted != null) {
+            if (tobeDeleted.val == key) {
+                break;
+            } else if (tobeDeleted.val > key) {
+                prev = tobeDeleted;
+                tobeDeleted = tobeDeleted.left;
+                left = true;
+                right = false;
+            } else {
+                prev = tobeDeleted;
+                tobeDeleted = tobeDeleted.right;
+                right = true;
+                left = false;
+            }
+        }
+
+        if (tobeDeleted == null) {
+            return root;
+        }
+        if (left && tobeDeleted.right != null) {
+            prev.left = tobeDeleted.right;
+            moveNodeToLast(prev.left, tobeDeleted.left);
+        } else if (left && tobeDeleted.left != null) {
+            prev.left = tobeDeleted.left;
+        } else if (left) {
+            prev.left = null;
+        }
+
+        if (right && tobeDeleted.right != null) {
+            prev.right = tobeDeleted.right;
+            moveNodeToLast(prev.right, tobeDeleted.left);
+        } else if (right && tobeDeleted.left != null) {
+            prev.right = tobeDeleted.left;
+        } else if (right) {
+            prev.right = null;
+        }
+
+        if (!left && !right) {
+            if (tobeDeleted.right != null) {
+                root = tobeDeleted.right;
+                moveNodeToLast(root, tobeDeleted.left);
+            } else if (tobeDeleted.left != null) {
+                root = tobeDeleted.left;
+            } else {
+                root = null;
+            }
+        }
+        return root;
+    }
+
+    private void moveNodeToLast(TreeNode source, TreeNode target) {
+        if (target == null) return;
+        TreeNode temp = source;
+        while (source != null) {
+            temp = source;
+            source = source.left;
+        }
+        temp.left = target;
+    }
+
+    public static void main(String[] args) {
+        DeleteNodeBinaryTree d = new DeleteNodeBinaryTree();
+//        TreeNode source = new TreeNode(4).leftAndRight(2, 7);
+//        source.left.leftAndRight(1, 3);
+//        TreeNode res = d.deleteNode(source, 8);
+//        TreeNode.print(res);
+
+        TreeNode source = new TreeNode(5).leftAndRight(3, 6);
+        source.left.leftAndRight(2, 4);
+        source.right.leftAndRight(null, 7);
+        TreeNode.print(d.deleteNode(source, 7));
+    }
+}
