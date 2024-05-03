75131cb24638410aadc40de7d1b76bac37625aa2
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2024-05-04 00:46:27 +0530

    Updated SearchInsertPosition.java

---
+++ src/main/java/org/sk/binarysearch/SearchInsertPosition.java
@@ -0,0 +1,25 @@
+package org.sk.binarysearch;
+
+public class SearchInsertPosition {
+    public static int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 2));
+        System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 7));
+    }
+}
