61f8a4055d13354cc252d5587ca73af7f5aa7d8f
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2024-05-18 20:15:49 +0530

    Updated PrefixSum

---
+++ src/main/java/org/sk/arraysAndHashing/PlusOne.java
@@ -0,0 +1,22 @@
+package org.sk.arraysAndHashing;
+
+import java.util.Arrays;
+
+public class PlusOne {
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(plusOne(new int[] {1, 2, 3})));
+        System.out.println(Arrays.toString(plusOne(new int[] {1, 9, 9})));
+    }
+}
