e7e3d34a4ee55190bcdc7d3622f0c4a310ddcf86
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2024-05-21 18:10:39 +0530

    Updated LongestCommonSubsequence solved

--- src/main/java/org/sk/dp/LongestCommonSubsequence.java
+++ src/main/java/org/sk/dp/LongestCommonSubsequence.java
@@ -15,8 +15,37 @@ public class LongestCommonSubsequence {
         return arr[arr.length - 1][arr[0].length - 1];
     }
 
+    static int[][] t;
+    public static int longestCommonSubsequenceViaRecursion(String text1, String text2) {
+        int m=text1.length(), n=text2.length();
+        char[] t1 = text1.toCharArray();
+        char[] t2 = text2.toCharArray();
+        t = new int[m+1][n+1];
+
+        return lcs(t1, t2, m, n);
+    }
+
+    private static int lcs(char[] t1, char[] t2, int i, int j){
+        if(t[i][j] > 0)
+            return t[i][j];
+
+        if(i == 0 || j == 0)
+            return 0;
+
+        int val = 0;
+        if(t1[i-1] == t2[j-1])
+            val = 1 + lcs(t1, t2, i-1, j-1);
+        else
+            val = Math.max(lcs(t1, t2, i-1, j), lcs(t1, t2, i, j-1));
+
+        t[i][j] = val;
+        return val;
+    }
+
     public static void main(String[] args) {
-        System.out.println(longestCommonSubsequence("ezupkr", "ubmrapg"));
+        System.out.println(longestCommonSubsequenceViaRecursion("ezupkr", "ubmrapg"));
+        System.out.println(longestCommonSubsequenceViaRecursion("abcde", "ace"));
     }
 
 }
+
