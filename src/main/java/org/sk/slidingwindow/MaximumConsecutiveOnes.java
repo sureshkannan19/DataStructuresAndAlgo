7912de8ea3f26e716674097bf2aeaff959b028e4
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2024-05-30 22:27:55 +0530

    MaximumConsecutiveOnes solved

---
+++ src/main/java/org/sk/slidingwindow/MaximumConsecutiveOnes.java
@@ -0,0 +1,31 @@
+package org.sk.slidingwindow;
+
+public class MaximumConsecutiveOnes {
+
+    public static int longestOnes(int[] nums, int k) {
+        int countZero = 0;
+        int max = 0;
+        int start = 0;
+        // 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1
+        for (int end = 0; end < nums.length; end++) {
+            //Count zeros
+            if (nums[end] == 0) {
+                countZero++;
+            }
+            //clean up if zero are more than k
+            while (countZero > k && start <= end) {
+                if (nums[start++] == 0) {
+                    countZero--;
+                }
+                max = Math.max(max, end - start + 1);
+            }
+        }
+        return max;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(longestOnes(new int[]{1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0}, 2));
+        System.out.println(longestOnes(new int[]{0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1}, 3));
+    }
+}
