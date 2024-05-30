a4a20b7ba66da3157c5df63692ea37737d740fea
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2024-05-30 22:38:40 +0530

    MaximumConsecutiveOnes solved

--- src/main/java/org/sk/slidingwindow/MaximumConsecutiveOnes.java
+++ src/main/java/org/sk/slidingwindow/MaximumConsecutiveOnes.java
@@ -1,24 +1,27 @@
 package org.sk.slidingwindow;
 
+import java.util.LinkedList;
+
 public class MaximumConsecutiveOnes {
 
     public static int longestOnes(int[] nums, int k) {
         int countZero = 0;
         int max = 0;
         int start = 0;
-        // 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1
+
+        // keep track of zeros
+        LinkedList<Integer> zeroPos = new LinkedList<>();
         for (int end = 0; end < nums.length; end++) {
-            //Count zeros
             if (nums[end] == 0) {
                 countZero++;
+                zeroPos.add(end);
             }
-            //clean up if zero are more than k
-            while (countZero > k && start <= end) {
-                if (nums[start++] == 0) {
-                    countZero--;
-                }
-                max = Math.max(max, end - start + 1);
+            if (countZero > k) {
+                countZero--;
+                // shift window begin position by polling first zero of current window
+                start = zeroPos.poll() + 1;
             }
+            max = Math.max(max, end - start + 1);
         }
         return max;
     }
