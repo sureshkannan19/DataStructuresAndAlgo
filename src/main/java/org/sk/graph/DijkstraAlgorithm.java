5ae6beecdeb4cd6f8aca46cac76a37be54d2cb22
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2024-06-15 11:04:52 +0530

    DijkstraAlgorithm - Network Delay time

--- src/main/java/org/sk/graph/DijkstraAlgorithm.java
+++ src/main/java/org/sk/graph/DijkstraAlgorithm.java
@@ -38,10 +38,8 @@ public class DijkstraAlgorithm {
     }
 
     public static void main(String[] args) {
-        System.out.println(networkDelayTimeViaDP(new int[][]{{1, 2, 1}}, 2, 1));
         System.out.println(networkDelayTime(new int[][]{{1, 2, 1}}, 2, 2));
         System.out.println(networkDelayTime(new int[][]{{2, 1, 1}, {2, 3, 1}, {3, 4, 1}}, 4, 2));
-        System.out.println(networkDelayTimeViaDP(new int[][]{{2, 1, 1}, {2, 3, 1}, {3, 4, 1}}, 4, 2));
         System.out.println(networkDelayTime(new int[][]{{2, 1, 1}, {2, 3, 1}, {3, 4, 1}}, 4, 2));
         System.out.println(networkDelayTime(new int[][]{{1, 2, 1}, {2, 3, 2}, {1, 3, 2}}, 3, 1));
     }
