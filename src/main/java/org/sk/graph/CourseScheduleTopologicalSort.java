ab96d049bffcb6ef2f6ebb572f0046e086b83878
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2024-06-26 22:30:23 +0530

    CourseScheduleTopologicalSort Refactored

--- src/main/java/org/sk/graph/CourseScheduleTopologicalSort.java
+++ src/main/java/org/sk/graph/CourseScheduleTopologicalSort.java
@@ -5,50 +5,46 @@ import java.util.*;
 public class CourseScheduleTopologicalSort {
     public static boolean canFinish(int numCourses, int[][] prerequisites) {
         if (prerequisites.length < 1) return true;
-
         Map<Integer, List<Integer>> preRequisiteCoursesMap = new HashMap<>();
         for (int[] courses : prerequisites) {
             preRequisiteCoursesMap.computeIfAbsent(courses[0], v -> new ArrayList<>()).add(courses[1]);
         }
 
         LinkedList<Integer> topologyOrder = new LinkedList<>();
-        int[] visited = new int[numCourses];
+        Map<Integer, Integer> topologicalOrderIndexMap = new HashMap<>();
+
         for (int[] prerequisite : prerequisites) {
             int currentCourse = prerequisite[0];
-            if (visited[currentCourse] == 0) {
-                dfs(preRequisiteCoursesMap, topologyOrder, visited, currentCourse);
+            if (!topologicalOrderIndexMap.containsKey(currentCourse)) {
+                dfs(preRequisiteCoursesMap, topologyOrder, currentCourse, topologicalOrderIndexMap);
             }
         }
-
-        Map<Integer, Integer> topologicalOrderIndex = new HashMap<>();
-        for (int i = 0; i < topologyOrder.size(); i++) {
-            topologicalOrderIndex.put(topologyOrder.get(i), i);
-        }
-
         for (int[] prerequisite : prerequisites) {
             int actualCourse = prerequisite[0]; // 0 , 1
             int prerequisiteCourse = prerequisite[1]; // 1 , 0
-            if (topologicalOrderIndex.get(actualCourse) >= topologicalOrderIndex.get(prerequisiteCourse)) {
+            if (topologicalOrderIndexMap.get(actualCourse) <= topologicalOrderIndexMap.get(prerequisiteCourse)) {
                 return false;
             }
         }
-//        System.out.println(topologyOrder);
         return true;
     }
 
     private static void dfs(Map<Integer, List<Integer>> preRequisiteCoursesMap,
-                            LinkedList<Integer> topologySorted, int[] visited, int at) {
-        visited[at] = 1;
+                            LinkedList<Integer> topologySorted, int at,
+                            Map<Integer, Integer> topologicalOrderIndex) {
+        topologicalOrderIndex.put(at, topologySorted.size());
         List<Integer> prerequisiteCourses = preRequisiteCoursesMap.get(at);
         if (prerequisiteCourses != null) {
             for (int i : prerequisiteCourses) {
-                if (visited[i] == 0) {
-                    dfs(preRequisiteCoursesMap, topologySorted, visited, i);
+                if (!topologicalOrderIndex.containsKey(i)) {
+                    dfs(preRequisiteCoursesMap, topologySorted, i, topologicalOrderIndex);
                 }
             }
         }
         topologySorted.push(at);
+        topologicalOrderIndex.put(at, topologySorted.size());
     }
+
     public static void main(String[] args) {
         System.out.println(canFinish(4, new int[][]{{0, 1}, {1, 2}, {2, 3}}));
         System.out.println(canFinish(2, new int[][]{{0, 1}, {1, 0}}));
